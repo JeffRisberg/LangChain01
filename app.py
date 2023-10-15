7f161d6c74a6184291090834f1a7d7e8e34c20d6
JeffRisberg <j.s.risberg@gmail.com>
2023-10-14 20:59:39 -0700

    filled in 'information'

--- app.py
+++ app.py
@@ -3,7 +3,27 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
 
-
+information = """
+"""
 
 if __name__ == "__main__":
     print("Hello LangChain")
+
+    summary_template = """
+      Given the information {information} about a person, I want to you create:
+      1. a short summary of the person
+      2. two interesting facts about the person
+
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables = "information", prompt_template = summary_template)
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    print(chain)
+
+
+
